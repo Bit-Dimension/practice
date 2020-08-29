68540c462debe86fc806c237e922ce9f9fe07144
Bit-Dimension <70371322+Bit-Dimension@users.noreply.github.com>
2020-08-29 08:11:49 +0800

    Update 两位数相加.cpp

--- 两位数相加.cpp
+++ 两位数相加.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
-{
-int num1=0;
+{int num1=0;
 int num2=0;
 int sum=0;
+printf("输入两个数字完成加法，数字间用空格隔开，输入完成后按回车执行\n");
 scanf("%d%d",&num1,&num2);
 sum=num1+num2;
-printf("sum=%d",sum);
+	printf("sum=%d\n",sum);
 	return 0;
 }
